0c811b4fdf71f730119f89407d887bc6415c7259
Mike Hoffert <mike.hoffert@gmail.com>
2021-06-30 16:14:29 -0600

    Use port 3366

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :twenty, Twenty.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :twenty, TwentyWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 3366],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
